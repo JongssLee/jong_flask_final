ca4332e79709786ac21aac29c8a12d23e608a4c5
JongsLee <jongseo12345@gmail.com>
2021-03-10 12:33:24 +0900

    config

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -22,7 +22,7 @@ migrate = Migrate()
 def create_app():
     app = Flask(__name__)
 # --------------------------------- [edit] ---------------------------------- #    
-    app.config.from_object(config)
+    app.config.from_envvar('APP_CONFIG_FILE')
 
     # ORM
     db.init_app(app)
